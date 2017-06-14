6533c5bfe3be9823830a16be5847605a8dfab810
openscreencast <info@openscreencast.de>
2017-06-14 20:18:02 +0200

    remove images, add icons

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ var data = [
             {
             name: "Python für Anfänger",
             gitbook_url: "https://openscreencast.gitbooks.io/python-fuer-anfaenger/content/",
-            image: "images/python.png",
+            image: "terminal",
             description: "Lerne in kurzen Schritten die Grundlagen der Programmiersprache Python.",
             book: "Screencast",
             book_category: "Programmierung"
@@ -11,7 +11,7 @@ var data = [
             {
             name: "Git für Einsteiger",
             gitbook_url: "https://openscreencast.gitbooks.io/git/content/",
-            image: "images/git.png",
+            image: "git",
             description: "Lerne die einfachsten Befehle der Versionsverwaltung Git.",
             book: "Screencast",
             book_category: "Programmierung"
@@ -27,7 +27,7 @@ var data = [
             {
             name: "HTML und CSS",
             gitbook_url: "https://openscreencast.gitbooks.io/html_und_css/content/",
-            image: "images/htmlundcss.png",
+            image: "html5",
             description: "Lerne die Grundbausteine von HTML und CSS kennen.",
             book: "Screencast",
             book_category: "Web"
