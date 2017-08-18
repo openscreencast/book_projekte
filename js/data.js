ff4924845f7c44fdfb0c779044eb6780fe82e0d1
openscreencast <info@openscreencast.de>
2017-08-18 15:29:49 +0200

    update data.js

--- js/data.js
+++ js/data.js
@@ -2,35 +2,33 @@ var data = [
 
             {
             name: "Python für Anfänger",
-            gitbook_url: "https://openscreencast.gitbooks.io/python-fuer-anfaenger/content/",
+            url: "https://www.openscreencast.de/books/python-fuer-anfaenger/",
             image: "terminal",
             description: "Lerne in kurzen Schritten die Grundlagen der Programmiersprache Python.",
             book: "Screencast",
-            book_category: "Programmierung"
+            category: "Programmierung",
+	    tags: [ "Programmierung", "Python" ],
+	    lang: "de"
             },
             {
             name: "Git für Einsteiger",
-            gitbook_url: "https://openscreencast.gitbooks.io/git/content/",
+            url: "https://www.openscreencast.de/books/git/",
             image: "git",
             description: "Lerne die einfachsten Befehle der Versionsverwaltung Git.",
             book: "Screencast",
-            book_category: "Programmierung"
+            category: "Programmierung",
+            tags: [ "Versionsverwaltung", "Git" ],
+	    lang: "de"
             },
-            /*{
-            name: "R - Programmierung",
-            gitbook_url: "https://openscreencast.gitbooks.io/r_programming/content/",
-            image: "images/r.png",
-            description: "Lerne die Grundlagen der Programmiersprache R.",
-            book: "Screencast",
-            book_category: "Programmierung"
-            },*/
             {
             name: "HTML und CSS",
-            gitbook_url: "https://openscreencast.gitbooks.io/html_und_css/content/",
+            url: "https://www.openscreencast.de/books/html_und_css/",
             image: "html5",
             description: "Lerne die Grundbausteine von HTML und CSS kennen.",
             book: "Screencast",
-            book_category: "Web"
+            category: "Web",
+            tags: [ "Web", "HTML", "CSS", "www" ],
+	    lang: "de"
             }
  
 ];
